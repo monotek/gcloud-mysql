5bf8e57e5b8dac09298af739fdd94e7ab4f1a9ca
André Bauer <monotek23@gmail.com>
2018-11-26 19:09:17 +0100

    fixed syntax error

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,14 @@
 
 set -e
 
+TIMESTAMP="$(date +'%Y%m%d%H%M')"
+BACKUP_DIR="/tmp"
+
 if [ "$1" = 'mysql-backup-all' ]; then
-
-    for DB in (mysql -h "${MYSQL_HOST}" -u ${MYSQL_USER} -p ${MYSQL_PASS} -e "SHOW DATABASES;"); do
+    for DB in $(mysql -h "${MYSQL_HOST}" -u ${MYSQL_USER} -p ${MYSQL_PASS} -e "SHOW DATABASES;"|egrep -v "(|)"); do
 	echo "backup ${DB}"
+        mysqldump --opt -h "${MYSQL_HOST}" -u ${MYSQL_USER} -p ${MYSQL_PASS} ${DB} | gzip > /${BACKUP_DIR}/${TIMESTAMP}_${DB}.sql.gz
     done
+else
+    ${1}
 fi
-
