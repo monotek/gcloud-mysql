800c016c9e9f003143a00baf7a69b9b39758178b
André Bauer <monotek@users.noreply.github.com>
2020-11-24 21:08:40 +0100

    updated image & ci (#3)
    * updated image & ci
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * use quay
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * fix shellcheck
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * fix more shellcheck
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * fix dockerfile
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * use fixed apk versions
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * renamed action filename & fix codespell
    
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # gcloud-mysql
-gcloud sdk + mysql client + rsync
+
+Alpine linux based Docker image which contains the following tools:
+
+* gcloud sdk
+* mysql client
+* rsync
+
+Useful for mysqldump in container environments like [Googles Kubernetes Engine](https://cloud.google.com/kubernetes-engine).
+
+## Docker image
+
+Docker image can be found on [quai.io](https://quay.io/repository/monotek/gcloud-mysql).
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ TIMESTAMP="$(date +'%Y%m%d%H%M')"
 BACKUP_DIR="/tmp"
 
 if [ "$1" = 'mysql-backup-all' ]; then
-    for DB in $(mysql -h "${MYSQL_HOST}" -u ${MYSQL_USER} -p ${MYSQL_PASS} -e "SHOW DATABASES;"|egrep -v "(|)"); do
+    for DB in $(mysql -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p "${MYSQL_PASS}" -e "SHOW DATABASES;"| grep -E -v "(|)"); do
 	echo "backup ${DB}"
-        mysqldump --opt -h "${MYSQL_HOST}" -u ${MYSQL_USER} -p ${MYSQL_PASS} ${DB} | gzip > /${BACKUP_DIR}/${TIMESTAMP}_${DB}.sql.gz
+        mysqldump --opt -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p "${MYSQL_PASS}" "${DB}" | gzip > /"${BACKUP_DIR}/${TIMESTAMP}_${DB}".sql.gz
     done
 else
     ${1}
